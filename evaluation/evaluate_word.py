9eb083f47766b5d2d07289214bfdd45019637b76
bpopeters <bpeters@coli.uni-saarland.de>
2022-03-17 18:27:03 +0000

    fix whitespace

--- evaluation/evaluate_word.py
+++ evaluation/evaluate_word.py
@@ -6,23 +6,23 @@ Author: Khuyagbaatar Batsuren
 Last Update: 03/14/2022
 """
 
-import numpy as np
 import codecs
+import numpy as np
 
 def distance(str1, str2):
     """Simple Levenshtein implementation for evalm."""
-    m = np.zeros([len(str2)+1, len(str1)+1])
+    m = np.zeros([len(str2) + 1, len(str1) + 1])
     for x in range(1, len(str2) + 1):
-        m[x][0] = m[x-1][0] + 1
+        m[x][0] = m[x - 1][0] + 1
     for y in range(1, len(str1) + 1):
-        m[0][y] = m[0][y-1] + 1
+        m[0][y] = m[0][y - 1] + 1
     for x in range(1, len(str2) + 1):
         for y in range(1, len(str1) + 1):
-            if str1[y-1] == str2[x-1]:
+            if str1[y - 1] == str2[x - 1]:
                 dg = 0
             else:
                 dg = 1
-            m[x][y] = min(m[x-1][y] + 1, m[x][y-1] + 1, m[x-1][y-1] + dg)
+            m[x][y] = min(m[x - 1][y] + 1, m[x][y - 1] + 1, m[x - 1][y - 1] + dg)
     return int(m[len(str2)][len(str1)])
 
 def read(fname, flag):
@@ -37,16 +37,16 @@ def read(fname, flag):
                 C[lemma] = tag
             else:
                 lemma, segments = line.split("\t")[0:2]
-            D[lemma] = segments.replace(' @@','|')
+            D[lemma] = segments.replace(' @@', '|')
     return D, C
 
 def printNumbers(T):
     answers, D_cat = T[0], T[1]
     cat_set = set(D_cat)
     for cat in cat_set:
-        print("\ncategory:\t" +cat)
-        print ("levenshtein:\t"+str(answers[cat]['distance'])+"\nprecision:\t"+str(answers[cat]['precision']))
-        print ("recall:\t"+str(answers[cat]['recall'])+"\nf-measure:\t"+str(answers[cat]['fmeasure']))
+        print("\ncategory:\t" + cat)
+        print("levenshtein:\t" + str(answers[cat]['distance']) + "\nprecision:\t" + str(answers[cat]['precision']))
+        print("recall:\t" + str(answers[cat]['recall']) + "\nf-measure:\t" + str(answers[cat]['fmeasure']))
 
 def eval_form(gold, guess, category):
     """ compute average accuracy and edit distance for word-level task """
@@ -54,7 +54,7 @@ def eval_form(gold, guess, category):
     tp, fp, total_m, total_g = {}, {}, {}, {}
     answers = {}
     if len(category) < 1:
-        category['']='all'
+        category[''] = 'all'
     for cat in category.values():
         tp[cat] = .0
         dist[cat] = .0
@@ -82,17 +82,17 @@ def eval_form(gold, guess, category):
         for b in guess_list:
             if b in gold_set:
                 if (cat in tp) is False:
-                    tp[cat]=.0
+                    tp[cat] = .0
                 tp[cat] += 1
         dist[cat] += distance(str1, str2)
         total[cat] += 1
 
     for cat in total.keys():
         answers[cat] = {}
-        answers[cat]['distance'] =round(dist[cat]/total[cat],2)
-        answers[cat]['precision'] = round((tp[cat]/total_g[cat])*100,2)
-        answers[cat]['recall'] = round((tp[cat]/total_m[cat])*100,2)
-        answers[cat]['fmeasure'] = round((2*answers[cat]['precision']*answers[cat]['recall']) / (answers[cat]['precision']+answers[cat]['recall']),2)
+        answers[cat]['distance'] = round(dist[cat] / total[cat], 2)
+        answers[cat]['precision'] = round((tp[cat] / total_g[cat]) * 100, 2)
+        answers[cat]['recall'] = round((tp[cat]/total_m[cat]) * 100, 2)
+        answers[cat]['fmeasure'] = round((2 * answers[cat]['precision'] * answers[cat]['recall']) / (answers[cat]['precision'] + answers[cat]['recall']), 2)
     return answers, total.keys()
 
 
@@ -108,5 +108,5 @@ if __name__ == "__main__":
     D_gold, D_cat = read(args.gold, args.category)
     D_guess = read(args.guess, False)[0]
     if args.category:
-        printNumbers(eval_form(D_gold, D_guess,D_cat))
-    printNumbers(eval_form(D_gold, D_guess,{}))
+        printNumbers(eval_form(D_gold, D_guess, D_cat))
+    printNumbers(eval_form(D_gold, D_guess, {}))
